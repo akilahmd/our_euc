77bdd024b5f2a87a9e8cc06e5d44846116076c5a
mera2 <merastat@gmail.com>
2016-10-04 11:54:26 +0200

    made some change on function documentation deleted one fail test

---
+++ tests/testthat/test.R
@@ -0,0 +1,24 @@
+library(testthat)
+library(oureuc)
+
+test_that("oureuc", {
+  print("Here is my tessssssssssssssssst")
+  b<-3
+  expect_equal(euclidian(9,12),b)
+
+}
+)
+test_that("oureuc", {
+  print("Here is my 2nd test")
+  b<-100
+  expect_equal(euclidian(100,1000),b)
+  
+}
+)
+test_that("oureuc", {
+  print("Here is my 3rd test")
+  b<-5
+  expect_equal(euclidian(5,35),b)
+  
+}
+)
